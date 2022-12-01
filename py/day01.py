915acf08acaf9e753afded8c721b45f26bd90f63
Joost Molenaar <jjm@j0057.nl>
2022-12-01 22:27:45 +0100

    [01] Calorie Counting: solve part one

---
+++ py/day01.py
@@ -0,0 +1,11 @@
+def parse(text):
+    return [[int(c) for c in elf.split('\n')] for elf in text.split('\n\n')]
+
+def day01a(text):
+    return max(sum(e) for e in parse(text))
+
+def test_01_ex1():
+    assert day01a('1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000') == 24000
+
+def test_01a(day01_text):
+    assert day01a(day01_text) == 68467
