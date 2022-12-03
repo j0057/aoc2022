917e4302f2d754df0f3ab51936724da4ebb9c6cb
Joost Molenaar <jjm@j0057.nl>
2022-12-03 11:35:17 +0100

    [03] Rucksack Reorganization: solve part two

--- py/day03.py
+++ py/day03.py
@@ -1,3 +1,8 @@
+from functools import reduce
+from operator import and_ as intersect
+
+chunks = lambda I, n: zip(*[iter(I)]*n)
+
 PRIO = {**{chr(ord('a') + v): v+1  for v in range(26)},
         **{chr(ord('A') + v): v+27 for v in range(26)}}
 
@@ -7,6 +12,9 @@ def split(R):
 def day03a(R):
     return sum(PRIO[({*a} & {*b}).pop()] for (a, b) in split(R))
 
+def day03b(R):
+    return sum(PRIO[reduce(intersect, C).pop()] for C in chunks(map(set, R), 3))
+
 EX = ['vJrwpWtwJgWrhcsFMMfFFhFp',
       'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
       'PmmdzqPrVvPwwTWBwg',
@@ -15,5 +23,7 @@ EX = ['vJrwpWtwJgWrhcsFMMfFFhFp',
       'CrZsJsPPZsGzwwsLwLmpwMDw']
 
 def test_03_ex1(): assert day03a(EX) == 157
+def test_03_ex2(): assert day03b(EX) == 70
 
 def test_03a(day03_lines): assert day03a(day03_lines) == 7863
+def test_03b(day03_lines): assert day03b(day03_lines) == 2488
