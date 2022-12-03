e02bead9edb742afabadd8aa4db43faee8092b95
Joost Molenaar <jjm@j0057.nl>
2022-12-03 11:37:40 +0100

    [01] Calorie Counting: improve code style

--- py/day01.py
+++ py/day01.py
@@ -7,14 +7,8 @@ def day01a(text):
 def day01b(text):
     return sum(sorted(sum(e) for e in parse(text))[-3:])
 
-def test_01_ex1():
-    assert day01a('1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000') == 24000
+def test_01_ex1(): assert day01a('1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000') == 24000
+def test_01_ex2(): assert day01b('1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000') == 45000
 
-def test_01_ex2():
-    assert day01b('1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000') == 45000
-
-def test_01a(day01_text):
-    assert day01a(day01_text) == 68467
-
-def test_01b(day01_text):
-    assert day01b(day01_text) == 203420
+def test_01a(day01_text): assert day01a(day01_text) == 68467
+def test_01b(day01_text): assert day01b(day01_text) == 203420
