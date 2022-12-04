0f1d6c392cacb0f4863cc749ddf20a1cacd9932c
Joost Molenaar <jjm@j0057.nl>
2022-12-04 07:14:44 +0100

    [04] Camp Cleanup: solve part two

--- py/day04.py
+++ py/day04.py
@@ -8,6 +8,13 @@ def day04a(L):
     return sum(a <= c <= d <= b or c <= a <= b <= d
                for (a, b, c, d) in parse(L))
 
+# In how many assignment pairs do the ranges overlap?
+def day04b(L):
+    return sum(a <= c <= b or a <= d < b or c <= a <= d or c <= b <= d
+               for (a, b, c, d) in parse(L))
+
 def test_04_ex1(): assert day04a(EX1) == 2
+def test_04_ex2(): assert day04b(EX1) == 4
 
 def test_04a(day04_lines): assert day04a(day04_lines) == 459
+def test_04b(day04_lines): assert day04b(day04_lines) == 779
