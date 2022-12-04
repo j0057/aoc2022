95ec87af2166a9ddc058a086b57d67dcd1bd0c3f
Joost Molenaar <jjm@j0057.nl>
2022-12-04 07:14:17 +0100

    [04] Camp Cleanup: solve part one

---
+++ py/day04.py
@@ -0,0 +1,13 @@
+EX1 = '2-4,6-8 2-3,4-5 5-7,7-9 2-8,3-7 6-6,4-6 2-6,4-8'.split()
+
+def parse(L):
+    return [tuple(int(x) for x in s.replace('-', ',').split(',')) for s in L]
+
+# In how many assignment pairs does one range fully contain the other?
+def day04a(L):
+    return sum(a <= c <= d <= b or c <= a <= b <= d
+               for (a, b, c, d) in parse(L))
+
+def test_04_ex1(): assert day04a(EX1) == 2
+
+def test_04a(day04_lines): assert day04a(day04_lines) == 459
