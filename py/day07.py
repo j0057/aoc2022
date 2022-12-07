c6cfd0c7045c0d1fd36519b1ec3611b85eac28c1
Joost Molenaar <jjm@j0057.nl>
2022-12-07 21:34:22 +0100

    [07] No Space Left On Device: solve part two

--- py/day07.py
+++ py/day07.py
@@ -34,6 +34,16 @@ def day07a(lines):
     tree = ('d', '', *parse_output(iter(lines)))
     return sum(size for size in find_dirs(tree) if size < 100_000)
 
+# Find the smallest directory that, if deleted, would free up enough space on
+# the filesystem to run the update. What is the total size of that directory?
+def day07b(lines):
+    tree = ('d', '', *parse_output(iter(lines)))
+    used = next(find_dirs(tree))
+    free = 70_000_000 - used
+    return min(size for size in find_dirs(tree) if size + free >= 30_000_000)
+
 def test_07_ex1(day07_ex_lines): assert day07a(day07_ex_lines(1)) == 95437
+def test_07_ex2(day07_ex_lines): assert day07b(day07_ex_lines(1)) == 24933642
 
 def test_07a(day07_lines): assert day07a(day07_lines) == 1513699
+def test_07b(day07_lines): assert day07b(day07_lines) == 7991939
