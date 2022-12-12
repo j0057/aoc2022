2993dc0eca7fdb776f3a9c8b33c9bb2e1d737cbe
Joost Molenaar <jjm@j0057.nl>
2022-12-12 13:12:34 +0100

    [12] Hill Climbing Algorithm: solve part two, stupidly

--- py/day12.py
+++ py/day12.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+from math import inf as INFINITY
 
 def parse(L):
     find = lambda c: next(C for C in ((y, s.find(c)) for (y, s) in enumerate(L)) if C[1] > -1)
@@ -22,13 +23,23 @@ def uniform_cost_search(G, source, target):
             if child not in reached or cost+1 < reached[child]:
                 reached[child] = cost+1
                 frontier.put((cost+1, child))
-    raise Exception('no path found')
+    return INFINITY
 
 # What is the fewest steps required to move from your current position to the
 # location that should get the best signal?
 def day12a(L):
     return uniform_cost_search(*parse(L))
 
+# What is the fewest steps required to move starting from any square with
+# elevation a to the location that should get the best signal?
+def day12b(L):
+    G, _, target = parse(L)
+    return min(uniform_cost_search(G, source, target)
+               for source in G
+               if G[source] == 1)
+
 def test_12_ex1(day12_ex_lines): assert day12a(day12_ex_lines(0)) == 31
+def test_12_ex2(day12_ex_lines): assert day12b(day12_ex_lines(0)) == 29
 
 def test_12a(day12_lines): assert day12a(day12_lines) == 468
+def test_12b(day12_lines): assert day12b(day12_lines) == 459
