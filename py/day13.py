8a7a395d9a460503d4a2fd0083fde7f14cad04ea
Joost Molenaar <jjm@j0057.nl>
2022-12-13 23:39:52 +0100

    [13] Distress Signal: copy @jrnijboer's better solution

--- py/day13.py
+++ py/day13.py
@@ -3,36 +3,26 @@ from functools import cmp_to_key
 def parse(L):
     return [eval(s) for s in L if s]
 
-def ordered(X, Y):
+def cmp(X, Y):
     match (X, Y):
-        case (list(X), int(Y)): Y = [Y]
-        case (int(X), list(Y)): X = [X]
+        case (list(X), int(Y)): return cmp(X, [Y])
+        case (int(X), list(Y)): return cmp([X], Y)
     match (X, Y):
-        case (int(a), int(b)) if a < b: return True
-        case (int(a), int(b)) if a > b: return False
-        case (int(a), int(b)) if a == b: return None
+        case (int(a), int(b)): return a - b
         case (list(A), list(B)):
-            check = (o for (a, b) in zip(A, B) if (o := ordered(a, b)) is not None)
-            if (result := next(check, None)) is not None:
-                return result
-            elif len(A) == len(B):
-                return None
-            else:
-                return len(A) < len(B)
-        case _: 1/0 # BANGK!
-    return False
+            check = (o for (a, b) in zip(A, B) if (o := cmp(a, b)))
+            return next(check, 0) or (len(A) - len(B))
 
 # Determine which pairs of packets are already in the right order. What is the
 # sum of the indices of those pairs?
 def day13a(L):
     chunks = lambda I, n: zip(*[iter(I)] * n)
-    return sum(i+1 for (i, (a, b)) in enumerate(chunks(parse(L), 2)) if ordered(a, b))
+    return sum(i+1 for (i, (a, b)) in enumerate(chunks(parse(L), 2)) if cmp(a, b) < 0)
 
 # Organize all of the packets into the correct order. What is the decoder key
 # for the distress signal?
 def day13b(L):
-    k = cmp_to_key(lambda a, b: -1 if ordered(a, b) else +1)
-    S = sorted([p := [[2]], q := [[6]], *parse(L)], key=k)
+    S = sorted([p := [[2]], q := [[6]], *parse(L)], key=cmp_to_key(cmp))
     return (S.index(p)+1) * (S.index(q)+1)
 
 def test_13_ex1(day13_ex_lines): assert day13a(day13_ex_lines(0)) == 13
