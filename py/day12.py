cfe2a1f5c6e4610b6581544a9133ad3803c01c05
Joost Molenaar <jjm@j0057.nl>
2022-12-13 20:59:58 +0100

    [12] Hill Climbing Algorithm: rewrite to not stupid solution
    (not to mention it running ~10x as fast)

--- py/day12.py
+++ py/day12.py
@@ -1,45 +1,42 @@
-from queue import PriorityQueue
 from math import inf as INFINITY
 
 def parse(L):
-    find = lambda c: next(C for C in ((y, s.find(c)) for (y, s) in enumerate(L)) if C[1] > -1)
-    return {(y, x): {'S': 1, 'E': 26}.get(c) or (ord(c)-ord('a')+1)
+    find = lambda c: next(x+y*1j for (y, s) in enumerate(L)
+                                 if (x := s.find(c)) > -1)
+    return {x+y*1j: {'S': 1, 'E': 26}.get(c) or (ord(c)-ord('a')+1)
             for (y, s) in enumerate(L)
             for (x, c) in enumerate(s)}, find('S'), find('E')
 
-def neighbours(G, y1, x1):
-    return [(y2, x2) for (y2, x2) in [(y1, x1+1), (y1+1, x1), (y1, x1-1), (y1-1, x1)]
-                     if (y2, x2) in G and G[y2, x2] - G[y1, x1] <= 1]
-
-def uniform_cost_search(G, source, target):
-    reached = {source: 0}
-    frontier = PriorityQueue()
-    frontier.put((0, source))
-    while not frontier.empty():
-        cost, node = frontier.get()
-        if node == target:
+def breadth_first_search(source, goal, expand):
+    explored = {source}
+    frontier = [(0, source)]
+    while frontier:
+        cost, node = frontier.pop(0)
+        if goal(node):
             return cost
-        for child in neighbours(G, *node):
-            if child not in reached or cost+1 < reached[child]:
-                reached[child] = cost+1
-                frontier.put((cost+1, child))
+        for child in expand(node):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.append((cost+1, child))
     return INFINITY
 
 # What is the fewest steps required to move from your current position to the
 # location that should get the best signal?
-def day12a(L):
-    return uniform_cost_search(*parse(L))
+def day12a(grid, source, target):
+    isgoal = lambda n: n == target
+    expand = lambda n: [c for c in [n+1, n+1j, n-1, n-1j] if grid.get(c, INFINITY)-grid[n] <= 1]
+    return breadth_first_search(source, isgoal, expand)
 
 # What is the fewest steps required to move starting from any square with
 # elevation a to the location that should get the best signal?
-def day12b(L):
-    G, _, target = parse(L)
-    return min(uniform_cost_search(G, source, target)
-               for source in G
-               if G[source] == 1)
+def day12b(G, source, target):
+    isgoal = lambda n: G[n] == 1
+    expand = lambda n: [c for c in [n+1, n+1j, n-1, n-1j] if G.get(c, -INFINITY)-G[n] >= -1]
+    return breadth_first_search(target, isgoal, expand)
 
-def test_12_ex1(day12_ex_lines): assert day12a(day12_ex_lines(0)) == 31
-def test_12_ex2(day12_ex_lines): assert day12b(day12_ex_lines(0)) == 29
+def test_12_ex1(day12_ex_lines): assert day12a(*parse(day12_ex_lines(0))) == 31
+def test_12_ex2(day12_ex_lines): assert day12b(*parse(day12_ex_lines(0))) == 29
 
-def test_12a(day12_lines): assert day12a(day12_lines) == 468
-def test_12b(day12_lines): assert day12b(day12_lines) == 459
+def test_12a(day12_lines): assert day12a(*parse(day12_lines)) == 468
+def test_12b(day12_lines): assert day12b(*parse(day12_lines)) == 459
