a06f6aeccf13dce6d83bbfac65ca12b2ac2d0826
Joost Molenaar <jjm@j0057.nl>
2022-12-17 09:17:37 +0100

    [15] Beacon Exclusion Zone: solve part two (in ~41s)

--- py/day15.py
+++ py/day15.py
@@ -24,8 +24,18 @@ def day15a(S, y):
                    if ay == y and any(x1 <= ax <= x2 for (x1, x2) in I)}
     return sum(x2-x1+1 for (x1, x2) in I) - len(SB)
 
+# Find the only possible position for the distress beacon. What is its tuning
+# frequency?
+def day15b(S, sq):
+    M = distances(S)
+    y, I = next((y, I) for y in range(sq) if len(I := exclude_y(M, y)) == 2)
+    return (I[0][1]+1) * 4_000_000 + y
+
 def test_15_ex1(day15_ex_lines): assert day15a(parse(day15_ex_lines(0)),  9) == 25
 def test_15_ex2(day15_ex_lines): assert day15a(parse(day15_ex_lines(0)), 10) == 26
 def test_15_ex3(day15_ex_lines): assert day15a(parse(day15_ex_lines(0)), 11) == 27
 
+def test_15_ex4(day15_ex_lines): assert day15b(parse(day15_ex_lines(0)), 20) == 56000011
+
 def test_15a(day15_lines): assert day15a(parse(day15_lines), 2_000_000) == 5716881
+def test_15b(day15_lines): assert day15b(parse(day15_lines), 4_000_000) == 10852583132904
